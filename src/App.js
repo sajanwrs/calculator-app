9630b6557b1f6292b597b7af40c5bb6c0328e9de
M. W. Rahman <sajanwrs@gmail.com>
2023-06-29 00:44:04 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -39,44 +39,43 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">A Simple Calculator</header>
-      <div>
-        <p id="calc-section" ref={resultRef}>
+    <div className="calculator-app">
+      <div className="output-section">
+        {/* <p id="calc-section" ref={resultRef}>
           {result}
-        </p>
-        <input
-          id="input-section"
-          pattern="[0-9]"
-          ref={inputRef}
-          type="number"
-          placeholder="Type a number"
-        />
-        <div id="button-section">
-          <button className="span-btn" id="all-clear" onClick={clearResult}>
-            AC
-          </button>
-          <button onClick={divide}>/</button>
-          <button onClick={multiply}>X</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button onClick={minus}>-</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button onClick={plus}>+</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button className="span-btn">0</button>
-          <button>.</button>
-          <button id="equals">=</button>
-          {/* 
-          <button onClick={clearInput}>clear</button>
-          <button onClick={clearResult}>clear result</button> */}
+        </p> */}
+        <div id="calc-section">123445</div>
+        <div id="input-section">
+          3445
+          {/* <input
+            id="input-section"
+            pattern="[0-9]"
+            ref={inputRef}
+            type="number"
+            placeholder="Type a number"
+          /> */}
         </div>
       </div>
+      <button className="span-btn" id="all-clear" onClick={clearResult}>
+        AC
+      </button>
+      <button id="del">DEL</button>
+      <button onClick={divide}>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button onClick={multiply}>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button onClick={minus}>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button onClick={plus}>+</button>
+      <button>0</button>
+      <button>.</button>
+      <button className="span-btn">=</button>
     </div>
   );
 }
